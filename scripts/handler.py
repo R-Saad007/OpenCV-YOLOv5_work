abf758032f1f7c3818573f19a7f4ec07f5005e3b
Rana Saad Imran <22100018@lums.edu.pk>
2023-04-04 16:35:32 +0500

    Update handler.py

--- scripts/handler.py
+++ scripts/handler.py
@@ -9,8 +9,7 @@ class handler():
     def __init__(self, vid):
         self.model = None                       # yolov5 model
         self.vid_path = vid                     # video path
-        self.frame_list = None                  # list of frames/images in video
-        self.inference_frame_list = None        # list of inferenced frames/images in video
+        self.frame_list = list()                # list of frames/images in video
 
     def load_model(self):
         # loading pytorch yolov5 model for inference
@@ -21,48 +20,64 @@ class handler():
         self.model.classes = [0]
 
     def frame_conversion(self):
-        # converting video to image frames for inference
-        # self.vid
-        # update frame_list
-        pass
-        
+        print("Starting Video Processing...")
+        # video capture object
+        cap = cv2.VideoCapture(self.vid_path)
+        # capturing first frame to check whether video exists for processing below
+        ret, frame = cap.read()
+        # video processing loop
+        while ret:
+            frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+            # appending each frame to the frame list
+            self.frame_list.append(frame)
+            # checking for user's exit command
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+            # error handling for last frame
+            try:
+                ret, frame = cap.read()                                
+            except Exception:
+                pass
+        # release the video capture object
+        cap.release()
+        # Closes all the windows currently opened.
+        cv2.destroyAllWindows()
+        print("Video Processing Completed!")
+        print('Number of frames: ',len(self.frame_list))
 
     def inference(self):
-        # inference on each video frame
-        # update inference_frame_list
-        pass
-
-    def save_output(self):
-        # storing each inferenced image frame
-        pass
-
-    def view_output(self):
-        # to view the inferenced image
-        pass
+        # inference on all video frames
+        print("Starting Frame Inferencing...")
+        results = self.model(self.frame_list)
+        print("Inferencing Completed!")
+        # saving results
+        results.save(args.output_path)
 
     def __del__(self):
         # object destructor
-        print("Handler destructor invoked")
+        self.model = None                                                   # yolov5 model
+        self.vid_path = None                                                # video path
+        self.frame_list = self.frame_list.clear()                           # list of frames/images in video
+        print("Handler destructor invoked!")
 
 # main function
 if __name__ == '__main__':
     # Argument from CLI
-    parser = argparse.ArgumentParser(description = 'Input file path required.')
+    parser = argparse.ArgumentParser(description = 'I/O file paths required.')
     parser.add_argument('-img_path', type = str, dest = 'img_path', required =True)
+    parser.add_argument('-output_path', type = str, dest = 'output_path', required =True)
     args = parser.parse_args()
 
     # For calculating execution time
     start = torch.cuda.Event(enable_timing=True)
     end = torch.cuda.Event(enable_timing=True)
     start.record()
-    print("Starting Video Inferencing")
 
     # whatever you are timing goes here
     vid_handler = handler(args.img_path)
     vid_handler.load_model()
     vid_handler.frame_conversion()
     vid_handler.inference()
-    vid_handler.save_output()
     del vid_handler
     end.record()
 
